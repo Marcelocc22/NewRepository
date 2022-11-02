4fbe01604f8978037334ef2fd7e5b7074b97b108
Marcelocc22 <celos9419@gmail.com>
2022-11-02 20:12:31 -0200

    news2
    new2

--- HTML_CSS_JS/JavaScript Training/JS_006/scripts.js
+++ HTML_CSS_JS/JavaScript Training/JS_006/scripts.js
@@ -1,2 +1,20 @@
+var a = window.document.getElementById("area");
+a.addEventListener("Click", Clicar);
+a.addEventListener("mouseout", Sair);
+a.addEventListener("mouseenter", Entrar);
 
+function Clicar() {
+    a.innerText = "Clicou"
+    a.style.backgroundColor = "yellow"
+}
+
+function Entrar() {
+    a.innerText = "Entrou"
+    a.style.backgroundColor = "cadetblue"
+}
+
+function Sair() {
+    a.innerText = "Sair"
+    a.style.backgroundColor = "cadetblue"
+}
 
