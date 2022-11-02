4f6fc9c4741dfe982eea95c90e054da845f88a44
Marcelocc22 <celos9419@gmail.com>
2022-11-02 19:20:14 -0200

    new
    news

--- HTML_CSS_JS/JavaScript Training/JS_005/scripts.js
+++ HTML_CSS_JS/JavaScript Training/JS_005/scripts.js
@@ -1,7 +1,9 @@
+
 var p1 = window.document.getElementsByTagName("p", "</br>")[0];
-window.document.write("Esta escrito assim: ", p1.innerText, "</br>");
+window.document.write("Esta escrito assim: " + p1.innerText, "</br>");
 window.document.write("Hello!", "</br>");
 window.document.write(window.document.characterSet, "</br>");
 window.document.write(window.navigator.appName, "</br>");
 window.document.write(window.URL());
- 
+
+
